f4f9ff456bd9816126323dd53072df7e1463c7f1
ezequiel sanchez <ezealesan17@gmail.com>
2022-08-18 14:29:58 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.sr= ScrollReveal();
+sr.reveal('.nav-main',{
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px',
+})
+
+sr.reveal('.presentacion',{
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px',
+
+})
+
+sr.reveal('.redes',{
+    duration:3000,
+    origin:'bottom',
+    distance:'-100px',
+})
